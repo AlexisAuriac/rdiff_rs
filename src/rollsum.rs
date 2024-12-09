9e3c98b724b4834c78f125d211c4f95f3a9ceac1
Alexis Auriac <alexis.auriac@epitech.eu>
2024-12-09 17:08:05 +0100

    fix rollsum::rotate; add rollsum tests

--- src/rollsum.rs
+++ src/rollsum.rs
@@ -52,8 +52,10 @@ impl Rollsum {
     }
 
     pub fn rotate(&mut self, outb: u8, inb: u8) {
-        self.s1 = self.s1.wrapping_add(inb.wrapping_sub(outb) as u16);
-        self.s2 = self.s2.wrapping_add(self.s1);
+        self.s1 = self.s1.wrapping_add((inb as u16).wrapping_sub(outb as u16));
+        self.s2 = self.s2.wrapping_add(self.s1.wrapping_sub(
+            (self.count as u16).wrapping_mul((outb as u16).wrapping_add(ROLLSUM_CHAR_OFFSET)),
+        ));
     }
 
     pub fn roll_in(&mut self, inb: u8) {
@@ -84,5 +86,101 @@ impl Rollsum {
         self.count = 0;
         self.s1 = 0;
         self.s2 = 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let r = Rollsum::new();
+        assert_eq!(0, r.digest());
+    }
+
+    #[test]
+    fn rollin_rollout() {
+        let mut r = Rollsum::new();
+
+        r.roll_in(222);
+        assert_eq!(0x00FD00FD, r.digest());
+        r.roll_in(11);
+        assert_eq!(0x02240127, r.digest());
+        r.roll_in(0);
+        assert_eq!(0x036A0146, r.digest());
+        r.roll_in(13);
+        assert_eq!(0x04DC0172, r.digest());
+        r.roll_in(7);
+        assert_eq!(0x06740198, r.digest());
+
+        r.roll_out(222);
+        assert_eq!(0x0183009B, r.digest());
+        r.roll_out(11);
+        assert_eq!(0x00DB0071, r.digest());
+        r.roll_out(0);
+        assert_eq!(0x007E0052, r.digest());
+
+        r.roll_in(1);
+        assert_eq!(0x00F00072, r.digest());
+    }
+
+    #[test]
+    fn update() {
+        let mut r = Rollsum::new();
+        let data = [222, 11, 0, 13, 7];
+        let more_data = [66, 171, 8];
+
+        r.update(&data);
+        assert_eq!(0x06740198, r.digest());
+        r.update(&more_data);
+        assert_eq!(0x0E1A02EA, r.digest())
+    }
+
+    #[test]
+    fn rotate() {
+        let mut r = Rollsum::new();
+        let data = [222, 11, 0, 13, 7];
+
+        r.update(&data);
+
+        r.rotate(222, 39);
+        assert_eq!(0x026400E1, r.digest());
+        r.rotate(11, 177);
+        assert_eq!(0x03190187, r.digest());
+        r.rotate(0, 0);
+        assert_eq!(0x04050187, r.digest());
+    }
+
+    #[test]
+    fn consistency() {
+        let data1 = [66, 1, 111, 54, 171, 12, 255, 199, 1, 2, 7, 12, 54, 43, 101];
+        let data2 = [4, 22, 66, 1, 111, 54, 171, 12, 255, 199, 1, 2, 7, 12, 54];
+
+        let mut rk1 = Rollsum::new();
+        rk1.update(&data1);
+
+        let mut rk2 = Rollsum::new();
+        for v in data2 {
+            rk2.roll_in(v);
+        }
+        rk2.rotate(4, 43);
+        rk2.roll_out(22);
+        rk2.roll_in(101);
+
+        assert_eq!(rk1.digest(), rk2.digest());
+    }
+
+    #[test]
+    fn rotate_byte_subtraction_bug() {
+        let mut rk1 = Rollsum::new();
+        rk1.roll_in(1);
+        assert_eq!(0x00200020, rk1.digest());
+
+        let mut rk2 = Rollsum::new();
+        rk2.roll_in(2);
+        rk2.rotate(2, 1);
+
+        assert_eq!(rk1.digest(), rk2.digest());
     }
 }
